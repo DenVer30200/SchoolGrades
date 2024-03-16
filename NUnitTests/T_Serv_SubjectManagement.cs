b51f97160ce4652961e1dd1cb46f03e5734b4dcb
Deniz Verstivschii <Deniz.Verstivschii.stud@ispascalcomandini.it>
2024-03-16 09:48:30 +0100

    prova

---
+++ NUnitTests/T_Serv_SubjectManagement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitDbTests
+{
+    internal class T_Serv_SubjectManagement
+    {
+    }
+}
